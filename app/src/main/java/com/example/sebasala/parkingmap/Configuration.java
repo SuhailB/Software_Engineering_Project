48da856fd96529ca6592cafd7477943c0102bec1
kylec0231 <44420319+kylec0231@users.noreply.github.com>
2018-11-24 20:23:36 -0600

    permit and timezone combination has been set up

--- app/src/main/java/com/example/sebasala/parkingmap/Configuration.java
+++ app/src/main/java/com/example/sebasala/parkingmap/Configuration.java
@@ -17,6 +17,8 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import java.util.TimeZone;
+
 public class Configuration extends AppCompatActivity {
 
 
@@ -49,7 +51,7 @@ public class Configuration extends AppCompatActivity {
         if(isServiceAvailabel()) startApp();
     }
 
-    public void onRadioButtonClicked(View view) {
+    /*public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
@@ -80,12 +82,15 @@ public class Configuration extends AppCompatActivity {
 
 
         }
-    }
+    }*/
 
     private void startApp()
     {
         Button btnLoad = (Button) findViewById(R.id.LoadParkingSpots);
-        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.AskPermit);
+        final RadioGroup radioGroupP = (RadioGroup) findViewById(R.id.AskPermit);
+        final RadioGroup radioGroupT = (RadioGroup) findViewById(R.id.TimeZone);
+
+/*
 //create the RadioButton
         RadioButton rb1 = new RadioButton(this);
 //set an id
@@ -102,6 +107,8 @@ public class Configuration extends AppCompatActivity {
         RadioButton rb4 = new RadioButton(this);
 //set an id
         rb1.setId(RBP4_ID);
+*/
+
 
 
 
@@ -112,9 +119,34 @@ public class Configuration extends AppCompatActivity {
             public void onClick(View v)
             {
 
-                int selectedPermit = radioGroup.getCheckedRadioButtonId();
-                RadioButton radioButton = (RadioButton) findViewById(selectedPermit);
-                String text = radioButton.getText().toString();
+                int selectedPermit = radioGroupP.getCheckedRadioButtonId();
+                RadioButton radioButtonP = (RadioButton) findViewById(selectedPermit);
+                String textP = "default";
+                if (radioGroupP.getCheckedRadioButtonId() != -1)
+                    textP = radioButtonP.getText().toString();
+                else
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Permit is needed to be checked",
+                            Toast.LENGTH_SHORT);
+
+                    toast.show();
+                }
+
+                int selectedTimeZone = radioGroupT.getCheckedRadioButtonId();
+                RadioButton radioButtonT = (RadioButton) findViewById(selectedTimeZone);
+
+                String textT = "default";
+                if (radioGroupT.getCheckedRadioButtonId() != -1)
+                    textT = radioButtonT.getText().toString();
+                else
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Timezone is needed to be checked",
+                            Toast.LENGTH_SHORT);
+
+                    toast.show();
+                }
 
                 /*Log.d(TAG, "selectedPermit is: " + selectedPermit);
 
@@ -123,18 +155,104 @@ public class Configuration extends AppCompatActivity {
 
                 //  onRadioButtonClicked(v);
 
-                if (text.contains("Faculty"))
-                    openMaps(1);
-                else if(text.contains("Resident"))
-                    openMaps(2);
-                else if(text.contains("Student"))
-                    openMaps(3);
-                else if(text.contains("Reserved"))
-                    openMaps(4);
-                else if(text.contains("No Permit"))
-                    openMaps(5);
+                if (textP.contains("Faculty")) {
+                    String permitFaculty = "Faculty";
+
+                    if(textT.contains("7:00AM"))
+                    {
+                        openMaps(1, permitFaculty);
+                    }
+                    else if(textT.contains("5:00PM"))
+                    {
+                        openMaps(2, permitFaculty);
+                    }
+                    else if(textT.contains("8:00PM"))
+                    {
+                        openMaps(3, permitFaculty);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if(textP.contains("Resident")) {
+                    String permitResident = "Resident";
+                    if(textT.contains("7:00AM"))
+                    {
+                        openMaps(1, permitResident);
+                    }
+                    else if(textT.contains("5:00PM"))
+                    {
+                        openMaps(2, permitResident);
+                    }
+                    else if(textT.contains("8:00PM"))
+                    {
+                        openMaps(3, permitResident);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if(textP.contains("Student")) {
+                    String permitStudent = "Student";
+                    if(textT.contains("7:00AM"))
+                    {
+                        openMaps(1, permitStudent);
+                    }
+                    else if(textT.contains("5:00PM"))
+                    {
+                        openMaps(2, permitStudent);
+                    }
+                    else if(textT.contains("8:00PM"))
+                    {
+                        openMaps(3, permitStudent);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if(textP.contains("Reserved")) {
+                    String permitReserved = "Reserved";
+                    if(textT.contains("7:00AM"))
+                    {
+                        openMaps(1, permitReserved);
+                    }
+                    else if(textT.contains("5:00PM"))
+                    {
+                        openMaps(2, permitReserved);
+                    }
+                    else if(textT.contains("8:00PM"))
+                    {
+                        openMaps(3, permitReserved);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if(textP.contains("No Permit")) {
+                    String permitNoPermit = "No Permit";
+                    if(textT.contains("7:00AM"))
+                    {
+                        openMaps(1, permitNoPermit);
+                    }
+                    else if(textT.contains("5:00PM"))
+                    {
+                        openMaps(2, permitNoPermit);
+                    }
+                    else if(textT.contains("8:00PM"))
+                    {
+                        openMaps(3, permitNoPermit);
+                    }
+                    else
+                    {
+
+                    }
+                }
                 else
-                    openMaps(6);
+                {}
             }
 
         });
@@ -247,10 +365,12 @@ public class Configuration extends AppCompatActivity {
         return false;
     }
 
-    public void openMaps(int buttonId)
+    public void openMaps(int buttonId, String PermitType)
     {
         Intent intent = new Intent(this, MapsActivity.class);
-        intent.putExtra(EXTRA_NUMBER,buttonId); //this adds buttonId to intent which sends it to mapsActivity
+        intent.putExtra(EXTRA_NUMBER,buttonId);//this adds buttonId to intent which sends it to mapsActivity
+        intent.putExtra(Intent.EXTRA_TEXT,PermitType);//this adds buttonId to intent which sends it to mapsActivity
+
         startActivity(intent);
     }
 }
